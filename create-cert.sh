fc1ddbe60297285b4631842ec9db64592f09baf6
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-10-25 09:48:50 +0200

    mkdir -p certs

--- create-cert.sh
+++ create-cert.sh
@@ -3,4 +3,5 @@
 # Macos:  ip=`ipconfig getifaddr en0` or check output of `scutil --dns`
 #         FQDN_HOST=`dig -x $ip +short | head -c-2`
 # manual: export FQDN_HOST=`hostname`.fritz.box
+mkdir -p certs
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/ociregistry.key -addext "subjectAltName = DNS:${FQDN_HOST}" -x509 -days 365 -out certs/ociregistry.crt -subj "/C=DE/ST=Baden-Wuertemberg/L=Walldorf/O=SAP/OU=ocm/CN=${FQDN_HOST}"
