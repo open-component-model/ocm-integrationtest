9f5b03e016a31bd362ec8e0e61caf1b108396cf9
jakobmoellerdev <jakob.moeller@sap.com>
2024-12-19 17:37:05 +0100

    Revert "chore: temporarily kill no cred test"
    This reverts commit 21bc71d1e77854b948a21b6ff714f5783918d346.

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,12 +1,17 @@
+import subprocess
+import time
+
+import pytest
+
 import ocmcli as ocm
 from ocm_fixture import ctx, ocm_no_config, OcmTestContext
 import util
 
 
-# pytestmark = pytest.mark.usefixtures("ocm_no_config")  # explicitely set no credentials to avoid auto fallback to docker confi
-# def test_transfer_without_credentials(ctx: OcmTestContext):
-#     with pytest.raises(ocm.OcmCliException, match='basic credential not found') as excinfo:
-#         ocm.execute_ocm(f'transfer artifacts gcr.io/google-containers/pause:3.2 {ctx.repo_dir}/images/pause:3.2')
+pytestmark = pytest.mark.usefixtures("ocm_no_config")  # explicitely set no credentials to avoid auto fallback to docker confi
+def test_transfer_without_credentials(ctx: OcmTestContext):
+    with pytest.raises(ocm.OcmCliException, match='basic credential not found') as excinfo:
+        ocm.execute_ocm(f'transfer artifacts gcr.io/google-containers/pause:3.2 {ctx.repo_dir}/images/pause:3.2')
 
 
 def test_transfer_with_credentials(ctx: OcmTestContext):
